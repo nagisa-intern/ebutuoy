e769fb1815c39643ae4b16e134aebcc34d97ad92
RyoTozawa <b1016031@fun.ac.jp>
2018-09-02 00:29:10 +0900

    fix ipaddress

--- webhook.py
+++ webhook.py
@@ -13,4 +13,4 @@ def on_push(data):
     print("Got push with: {0}".format(data))
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=83)
+    app.run(debug=True, host="0.0.0.0", port=80)
