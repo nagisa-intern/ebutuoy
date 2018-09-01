8a8177a2074a1df7ec718b7aab829b52a1aa6068
RyoTozawa <b1016031@fun.ac.jp>
2018-09-02 00:43:32 +0900

    set deploy

--- webhook.py
+++ webhook.py
@@ -1,5 +1,6 @@
 from github_webhook import Webhook
 from flask import Flask
+import subprocess
 
 app = Flask(__name__)  # Standard Flask app
 webhook = Webhook(app) # Defines '/postreceive' endpoint
@@ -11,6 +12,7 @@ def hello_world():
 @webhook.hook()        # Defines a handler for the 'push' event
 def on_push(data):
     print("Got push with: {0}".format(data))
+    res = subprocess.call('git pull ; git checkout -f infra ; docker-compose up -d')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
