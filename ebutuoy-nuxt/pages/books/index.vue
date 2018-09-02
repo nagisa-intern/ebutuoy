5b3cb27a4d7bb67c57849f6754188462bef323e1
yodaaa <yodaaa@MacBookProyodaaa.local>
2018-09-02 13:55:48 +0900

    url 修正

--- ebutuoy-nuxt/pages/books/index.vue
+++ ebutuoy-nuxt/pages/books/index.vue
@@ -9,7 +9,7 @@
     <section class="container">
       <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
         <el-menu-item index="1"><a href="../timeline" target="_self">タイムライン</a></el-menu-item>
-        <el-menu-item index="2"><a href="./" target="_self">本から選ぶ</a></el-menu-item>
+        <el-menu-item index="2"><a href="../books" target="_self">本から選ぶ</a></el-menu-item>
         <el-submenu index="3">
           <template slot="title">アカウント</template>
           <el-menu-item index="2-1"><a href="../users/1" target="_self">プロフィール</a></el-menu-item>
