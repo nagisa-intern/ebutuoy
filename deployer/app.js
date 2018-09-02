041856cbb8f555b00c46c8dfc2a98c8278c79682
RyoTozawa <b1016031@fun.ac.jp>
2018-09-02 11:30:24 +0900

    add res end

--- deployer/app.js
+++ deployer/app.js
@@ -11,7 +11,7 @@ const BASE_DIR = process.env.BASE_DIR
 
 const server = http.createServer((req, res) => {
   handler(req, res, (req, res) => {
-    return
+    res.end('Not Found')
   })
 }).listen(3000)
 
